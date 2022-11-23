6b219becbc8bdd1c4b5828a89c5c1ee2f19307b4
shengyaqingfeng <shengyaqingfeng@gmail.com>
2022-11-23 16:13:08 +0800

    update install shell

--- install/install.sh
+++ install/install.sh
@@ -1,9 +1,11 @@
-sudo apt-get install lib32gcc1 screen
+sudo apt update
+dpkg --add-architecture i386
+sudo apt update
+sudo apt-get install lib32gcc1 screen libsdl2-2.0-0:i386 libcurl4-gnutls-dev:i386
 mkdir ~/steamcmd
 cd ~/steamcmd
 wget https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz
 tar -xvzf steamcmd_linux.tar.gz
-sudo apt install libsdl2-2.0-0:i386
-cp ~/steamcmd/linux32/libstdc++.so.6 ~/dst/bin/lib32/
-sudo apt-get install libcurl4-gnutls-dev:i386
 ./steamcmd.sh +force_install_dir ~/dst +login anonymous +app_update 343050 validate +quit
+cp ~/steamcmd/linux32/libstdc++.so.6 ~/dst/bin/lib32/
+reboot
